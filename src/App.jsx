626debd050f0ed304b3bccf4e536c9aa1269a040
ayush <ayushshreyakar0802@gmail.com>
2023-05-22 12:38:41 +0530

    extract data fetching logic into a custom hook useShowById

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from 'react-query'
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
@@ -29,6 +34,7 @@ function App() {
       <Route path="contact-us" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -3,25 +3,32 @@ import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
 
 
+const useShowById = (showId) => {
+  const [showData, setShowData] = useState(null);
+  const [showError, setShowError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try{
+        const data = await getShowById(showId);
+        setShowData(data);
+      } catch (err) {
+        setShowError(err);
+      }
+    }
+
+    fetchData();
+  }, [showId]);
+
+  return {showData, showError}
+
+}
 
 const Show = () => {
     const { showId } = useParams();
+    const {showData, showError} = useShowById(showId)
 
-    const [showData, setShowData] = useState(null);
-    const [showError, setShowError] = useState(null);
-
-    useEffect(() => {
-      async function fetchData() {
-        try{
-          const data = await getShowById(showId);
-          setShowData(data);
-        } catch (err) {
-          setShowError(err);
-        }
-      }
-
-      fetchData();
-    }, [showId]);
+   
 
     if(showError) {
       return <div>We have an error: {showError.message}</div>;
