7aee2c99c8f9413197fe4379fac27d199a67fe31
ayush <ayushshreyakar0802@gmail.com>
2023-05-24 15:35:21 +0530

    added initial logic to star shows with useReducer. created usePersistedReducer to sync state with local storage

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,7 +1,7 @@
 //import { Link } from "react-router-dom";
 
 
-const ShowCard = ({ name, image, id, summary }) => {
+const ShowCard = ({ name, image, id, summary, onStarMeClick }) => {
    
     const summaryStripped = summary ? summary.split(" ").slice(0, 10).join(' ').replace(/<.+?>/g, '') : 'No description'
    
@@ -14,7 +14,9 @@ const ShowCard = ({ name, image, id, summary }) => {
 
         <div>
             <a href={`/show/${id}` } target="_blank" rel="noreferrer">Read more</a>
-            <button type="button">Star me</button>
+            <button type="button" onClick={() => onStarMeClick(id)}>
+                Star me
+            </button>
         </div>
     </div>
 );
